e46d5de8bb0aedd857c83620af87de7a071342b0
Swivro Robot <54741819+swivro-robot@users.noreply.github.com>
2022-03-12 13:36:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 function generate() {
-  var generatedPassword = document.getElementById(generatedPassword);
+  var generatedPassword = document.getElementById("generatedPassword");
   generatedPassword.value = ("this is a test");
 }
